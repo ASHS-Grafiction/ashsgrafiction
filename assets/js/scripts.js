9c1c3022b9e682b6735a5f80ab3647d7950fdbd3
Jacoder23 <Jacoder23@users.noreply.github.com>
2021-05-11 02:52:02 +0800

    Added animations, changed page titles, and removed a comic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,14 +12,14 @@ $( ".card-body" ).hide();
 
 $(".card").hover(function() {
     if(thisisaphone()){
-        $( ".card-body" ).hide();
+        $( ".card-body" ).slideUp(200);
         firstTap = true;
     }
-   $(this).children(".card-body").show();
+   $(this).children(".card-body").slideDown(300);
 });
 
 if(!thisisaphone()){
     $(".card").mouseout(function() {
-       $(this).children(".card-body").hide();
+       $(this).children(".card-body").slideUp(200);
     });
 }
